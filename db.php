e32e46ec2e5db06cc54d9df1378d0be8c62f6fd5
AdriFC <adri86vigo@gmail.com>
2021-04-30 14:41:19 +0200

    Creación CRUD PHP MySQL

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+//Connection with the bd
+$conn = mysqli_connect('localhost', 'root', '', 'php-crud-mysql');
+
+// if (isset($conn)){
+//     echo "DB is connected";
+// }
+
+
+?>
